b562edc6044ba2cb74ba549d45f547ea2112518f
chenyu <535509852@qq.com>
2019-05-29 01:09:47 +0800

    push

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-console.log("Script loaded!");
+console.log("加载sw脚本");
 var cacheStorageKey = "minimal-pwa-8";
 
 var cacheList = ["/", "index.html", "main.css", "e.png", "pwa-fonts.png"];
 
 self.addEventListener("install", function(e) {
-  console.log("Cache event!");
+  console.log("安装事件，注册后触发只触发一次");
   e.waitUntil(
     caches
       .open(cacheStorageKey)
       .then(function(cache) {
-        console.log("Adding to Cache:", cacheList);
+        console.log("加入缓存", cacheList);
         return cache.addAll(cacheList);
       })
       .then(function() {
-        console.log("Skip waiting!");
+        console.log("跳过等待");
         return self.skipWaiting();
       })
   );
@@ -38,7 +38,7 @@ self.addEventListener("install", function(e) {
 // });
 
 self.addEventListener("activate", function(e) {
-  console.log("Activate event");
+  console.log("激活事件");
   e.waitUntil(
     caches
       .keys()
@@ -46,7 +46,7 @@ self.addEventListener("activate", function(e) {
         return Promise.all(
           cacheNames.map(name => {
             if (name !== cacheStorageKey) {
-              console.log("[ServiceWorker] Removing old cache", name);
+              console.log("移除旧缓存", name);
               return caches.delete(name);
             }
           })
@@ -60,14 +60,14 @@ self.addEventListener("activate", function(e) {
 });
 
 self.addEventListener("fetch", function(e) {
-  // console.log('Fetch event:', e.request.url)
+  console.log("Fetch事件", e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       if (response != null) {
-        console.log("Using cache for:", e.request.url);
+        console.log("读取缓存:", e.request.url);
         return response;
       }
-      console.log("Fallback to fetch:", e.request.url);
+      console.log("无缓存直接请求", e.request.url);
       return fetch(e.request.url);
     })
   );
